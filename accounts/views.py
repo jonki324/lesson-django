4487fcf6716828ae5f736ae836caac11c3fb0c75
jonki324 <jonki324@gmail.com>
2020-07-24 18:01:32 +0900

    テンプレート追加

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World')
+        return render(request, 'accounts/index.html', {'msg': 'templete'})
 
 index = IndexView.as_view()
